654ea0ed11a88658254bf580f5aa96ebc9a12391
Paladugu-Vinod <vinodpaladugu53@gmail.com>
2023-11-08 18:43:05 +0530

    commit

---
+++ Array.js
@@ -0,0 +1,9 @@
+var str = 'Hello! Welocme to my world.';
+const reverseSentence = str => {
+   const arr = str.split(" ");
+   const reversed = arr.map(el => {
+      return el.split('').reverse().join("");
+   });
+   return reversed.join(" ");
+};
+console.log(reverseSentence(str));
